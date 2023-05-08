75e2190fd528dd884af541bbe9c99fdb33c5fb44
RedFord <stiozhkaolexiy@gmail.com>
2023-05-08 10:17:26 +0300

    Мінорні фікси

--- client/cash/src/components/elements/workingArea/WorkingArea.tsx
+++ client/cash/src/components/elements/workingArea/WorkingArea.tsx
@@ -2,23 +2,15 @@ import './WorkingArea.sass';
 import List from '../list/List';
 import Statistics from '../statistics/Statistics';
 import Calculator from '../calculator/Calculator';
-import { Component } from 'react';
 
-interface WorkingAreaProps {
-	getBool: () => void;
-}
-
-class WorkingArea extends Component<WorkingAreaProps> {
-	render() {
-		const { getBool } = this.props;
-		return (
-			<div className="working-area">
-				<List changeBool={getBool} />
-				<Statistics />
-				<Calculator />
-			</div>
-		)
-	}
-}
-
+const WorkingArea = ({getBool}: any) => {
+    return (
+        <div className="working-area">
+          <List changeBool={getBool}/>
+          <Statistics />
+          <Calculator />
+        </div>
+    );
+};
+  
 export default WorkingArea;
